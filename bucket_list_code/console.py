fadba558ff9ceb805f4aa6fb2c9fd3f2170a14ba
Karen Stewart <kastewart2002@googlemail.com>
2021-10-03 15:48:19 +0100

    CRUD for city and country writen

--- bucket_list_code/console.py
+++ bucket_list_code/console.py
@@ -5,11 +5,23 @@ from models.country import Country
 import repositories.city_repository as city_repository 
 import repositories.country_repository as country_repository
 
+city_repository.delete_all()
+country_repository.delete_all()
+
+
 country1 = Country("Spain")
 country_repository.save(country1)
+country2 = Country("Italy")
+country_repository.save(country2)
 
 city1 = City("barcelona", False, country1 )
 city_repository.save(city1)
+city2 = City("Astorga", False, country1)
+city_repository.save(city1)
+city3 = City("Milan", True, country2)
+city_repository.save(city2)
+
+
 
 
 
--- bucket_list_code/repositories/city_repository.py
+++ bucket_list_code/repositories/city_repository.py
@@ -14,5 +14,42 @@ def save(city):
     city.id = id
     return city
 
+def select_all():
+    cities = []
 
+    sql = "SELECT * FROM cities"
+    results = run_sql(sql)
+
+    for row in results:
+        country = country_repository.select(row['country_id'])
+        city = City(row['city_name'], row['visited'], country, row['id'])
+        cities.append(city)
+    return cities
+
+def select(id):
+    city = None
+    sql = "SELECT * FROM cities WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        country = country_repository.select(result['county_id'])
+        city = City(result['city_name'], result['visited'], country, result['id'])
+    return city
+
+
+def delete_all():
+    sql = "DELETE FROM cities"
+    run_sql(sql)
+
+def delete(id):
+    sql = "DELETE FROM cities WHERE id = %s"
+    values = [id]
+    run_sql(sql, values)
+
+def update(city):
+    sql = "UPDATE cities SET (city_name, visited, country_id) = (%s, %s, %s) WHERE id = %s"
+    values = [city.city_name, city.visited, city.country.id, city.id]
+    print(values)
+    run_sql(sql, values)
 
